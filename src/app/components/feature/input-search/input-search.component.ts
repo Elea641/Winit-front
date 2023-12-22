389a0547104df24aadd16befcbbd8e2bfb8dace5
Elea641 <eleavoliotis64@gmail.com>
2023-12-22 17:25:23 +0100

    wip sidebar

---
+++ src/app/components/feature/input-search/input-search.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-input-search',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
+  templateUrl: './input-search.component.html',
+  styleUrls: ['./input-search.component.scss'],
+})
+export class InputSearchComponent {
+  value = '';
+}
