2acd77d6b863bc28f4d97586f2663cf1ea7ef039
KevinDavoust <kevin.davoust@orange.fr>
2023-12-22 12:35:58 +0100

    finished card style

--- src/app/components/ui/carousel/carousel.component.ts
+++ src/app/components/ui/carousel/carousel.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Tournament } from 'src/app/tournament/models/tournament.model';
+import { Observable, map } from 'rxjs';
+import { TournamentService } from 'src/app/tournament/shared/tournament.service';
+import { TournamentCardComponent } from '../tournament-card/tournament-card.component';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TournamentCardComponent],
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent {
+  
+  tournaments$!: Observable<Tournament[]>;
 
+  constructor(private tournamentService: TournamentService) {}
+
+  ngOnInit(): void {
+    this.tournaments$ = this.tournamentService
+      .getAllTournaments()
+      .pipe(map((data) => data));
+  }
 }
