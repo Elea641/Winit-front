1e7433bf00d391bdf2a410fdb19379bc990e18f6
KevinDavoust <kevin.davoust@orange.fr>
2023-12-22 17:24:04 +0100

    swiperjs echec

--- src/app/components/ui/carousel/carousel.component.ts
+++ src/app/components/ui/carousel/carousel.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Tournament } from 'src/app/tournament/models/tournament.model';
 import { Observable, map } from 'rxjs';
 import { TournamentService } from 'src/app/tournament/shared/tournament.service';
 import { TournamentCardComponent } from '../tournament-card/tournament-card.component';
+
 
 @Component({
   selector: 'app-carousel',
@@ -12,10 +13,10 @@ import { TournamentCardComponent } from '../tournament-card/tournament-card.comp
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit{
   
   tournaments$!: Observable<Tournament[]>;
-
+  
   constructor(private tournamentService: TournamentService) {}
 
   ngOnInit(): void {
