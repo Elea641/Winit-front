f5d47fb6ad476c28bc24bb7a7c0d7a870b3fc02d
KevinDavoust <kevin.davoust@orange.fr>
2023-12-22 10:51:50 +0100

    created tournament-card and carousel components

---
+++ src/app/components/ui/tournament-card/tournament-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tournament-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tournament-card.component.html',
+  styleUrls: ['./tournament-card.component.scss']
+})
+export class TournamentCardComponent {
+
+}
