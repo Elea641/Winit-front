295f35ffa16bc82c56a4f7d542e70f1e017611aa
KevinDavoust <kevin.davoust@orange.fr>
2024-01-15 15:08:18 +0100

    test

--- src/app/home/pages/home-page/home-page.component.ts
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -1,34 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
 import { CarouselComponent } from 'src/app/components/ui/carousel/carousel.component';
-=======
 import { SidebarComponent } from 'src/app/components/ui/sidebar/sidebar.component';
 import { BreakpointService } from 'src/app/components/shared/breakpoint.service';
->>>>>>> 4d894b04a192d1d3bab2b7238748a2f717a4044e
 
 @Component({
-  selector: 'app-home-page',
-  standalone: true,
-<<<<<<< HEAD
-  imports: [CommonModule, CarouselComponent],
-=======
-  imports: [CommonModule, SidebarComponent],
->>>>>>> 4d894b04a192d1d3bab2b7238748a2f717a4044e
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.scss'],
+    selector: 'app-home-page',
+    standalone: true,
+    templateUrl: './home-page.component.html',
+    styleUrls: ['./home-page.component.scss'],
+    imports: [CommonModule, CarouselComponent, SidebarComponent]
 })
 export class HomePageComponent {
-<<<<<<< HEAD
-  
-=======
-  isMobile: boolean = false;
 
+  isMobile: boolean = false;
   constructor(private breakpointService: BreakpointService) {
     this.isMobile = this.breakpointService.isMobileDevice();
     this.breakpointService.isMobileChanged.subscribe((isMobile) => {
       this.isMobile = isMobile;
     });
   }
->>>>>>> 4d894b04a192d1d3bab2b7238748a2f717a4044e
 }
