41ca363beaec7a85a580a3830d1097dbe63d9ac5
Rémi Meslin <remi.meslin@guarani.fr>
2024-01-17 14:30:12 +0100

    [FEATURE] Ajout route création tournoi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { TournamentPageComponent } from './tournament/pages/tournament-page/tour
 import { HomePageComponent } from './home/pages/home-page/home-page.component';
 import { ContactPageComponent } from './contact/pages/contact-page/contact-page.component';
 import { ProfilePageComponent } from './profile/pages/profile-page/profile-page.component';
+import { TournamentFormComponent } from './tournament/components/feature/tournament-form/tournament-form.component';
 
 export const routes: Routes = [
   { path: 'profile', component: ProfilePageComponent },
   { path: 'contact', component: ContactPageComponent },
   { path: 'tournament', component: TournamentPageComponent },
+  { path: 'tournament/create', component: TournamentFormComponent},
   { path: '', component: HomePageComponent },
   { path: '**', redirectTo: '' },
 ];
