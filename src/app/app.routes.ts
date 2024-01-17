4381c92ec7de18a2900e94ef7aa10b0f794137dd
KevinDavoust <kevin.davoust@orange.fr>
2024-01-17 12:37:27 +0100

    login form template WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { TournamentPageComponent } from './tournament/pages/tournament-page/tour
 import { HomePageComponent } from './home/pages/home-page/home-page.component';
 import { ContactPageComponent } from './contact/pages/contact-page/contact-page.component';
 import { ProfilePageComponent } from './profile/pages/profile-page/profile-page.component';
+import { LoginPageComponent } from './login/pages/login-page/login-page.component';
 
 export const routes: Routes = [
   { path: 'profile', component: ProfilePageComponent },
+  { path: 'login', component: LoginPageComponent },
   { path: 'contact', component: ContactPageComponent },
   { path: 'tournament', component: TournamentPageComponent },
   { path: '', component: HomePageComponent },
