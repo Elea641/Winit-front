492303c06a729adad119cf94f370379e7ac0d7a7
COLINE GUERIN <guerin.coline@gmail.com>
2024-01-17 09:42:28 +0100

    created user model

---
+++ src/app/auth/models/user.model.ts
@@ -0,0 +1,10 @@
+import {Sport} from "./sport.model";
+
+export class User {
+  firstName?: string;
+  lastName?: string;
+  sport?: Sport;
+  email?: string;
+  password?: string;
+  confirmPassword?: string;
+}
