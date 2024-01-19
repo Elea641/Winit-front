ab59e22c8b5dabed3b89319e7f8d94c213ed5216
COLINE GUERIN <guerin.coline@gmail.com>
2024-01-19 10:41:49 +0100

    tournament list

--- src/app/tournament/components/feature/tournament-list/tournament-list.component.ts
+++ src/app/tournament/components/feature/tournament-list/tournament-list.component.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { Tournament } from 'src/app/tournament/models/tournament.model';
 import { Observable, map } from 'rxjs';
 import { TournamentService } from 'src/app/tournament/shared/tournament.service';
+import {TournamentCardComponent} from "../../../../components/ui/tournament-card/tournament-card.component";
+import {CarouselComponent} from "../../../../components/ui/carousel/carousel.component";
+import {SidebarComponent} from "../../../../components/ui/sidebar/sidebar.component";
+import {MatDividerModule} from "@angular/material/divider";
 
 @Component({
   selector: 'app-tournament-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TournamentCardComponent, CarouselComponent, SidebarComponent, MatDividerModule],
   templateUrl: './tournament-list.component.html',
   styleUrls: ['./tournament-list.component.scss'],
 })
