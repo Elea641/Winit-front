a20db5c3d248a32b9be52a99f08bd519c31d9999
COLINE GUERIN <guerin.coline@gmail.com>
2024-01-19 10:40:53 +0100

    Homepage titles

--- src/app/home/pages/home-page/home-page.component.ts
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { CarouselComponent } from 'src/app/components/ui/carousel/carousel.component';
 import { SidebarComponent } from 'src/app/components/ui/sidebar/sidebar.component';
 import { BreakpointService } from 'src/app/components/shared/breakpoint.service';
+import {MatDividerModule} from "@angular/material/divider";
 
 @Component({
     selector: 'app-home-page',
     standalone: true,
     templateUrl: './home-page.component.html',
     styleUrls: ['./home-page.component.scss'],
-    imports: [CommonModule, CarouselComponent, SidebarComponent]
+    imports: [CommonModule, CarouselComponent, SidebarComponent, MatDividerModule]
 })
 export class HomePageComponent {
 
