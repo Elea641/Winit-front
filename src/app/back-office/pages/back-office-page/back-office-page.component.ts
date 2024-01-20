753aba832f13d65bd64cf3cc641db013f249b7b7
Elea641 <eleavoliotis64@gmail.com>
2024-01-20 11:17:10 +0100

    add component back-office

---
+++ src/app/back-office/pages/back-office-page/back-office-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-back-office-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './back-office-page.component.html',
+  styleUrls: ['./back-office-page.component.scss']
+})
+export class BackOfficePageComponent {
+
+}
