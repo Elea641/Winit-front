16ca3a5cda6fe0715001549dc6b46f4f79212868
Elea641 <eleavoliotis64@gmail.com>
2024-01-25 09:05:38 +0100

    fixed sidebar

--- src/app/components/ui/sidebar/sidebar.component.ts
+++ src/app/components/ui/sidebar/sidebar.component.ts
@@ -30,7 +30,6 @@ import { BreakpointService } from '../../../shared/breakpoint.service';
     InputSearchComponent,
   ],
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
   @ViewChild('drawer') drawer!: MatDrawer;
