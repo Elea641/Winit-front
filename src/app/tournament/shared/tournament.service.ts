0d2b88dc5d7561e1af85667d98d4b27378c7bb6e
Rémi Meslin <remi.meslin@guarani.fr>
2024-02-15 16:38:51 +0100

    [FEATURE] Ajout méthode d'envoi api création tournoi

--- src/app/tournament/shared/tournament.service.ts
+++ src/app/tournament/shared/tournament.service.ts
@@ -9,6 +9,7 @@ import { TournamentDetails } from '../models/tournament-details.model';
 })
 export class TournamentService {
   private tournamentDataUrl = '../assets/list-tournament.model.json';
+  private apiUrl = 'http://localhost:8080/api/tournament/';
   private tournamentByIdDataUrl = '../assets/tournament-details.model.json';
 
   constructor(private http: HttpClient) {}
@@ -17,6 +18,12 @@ export class TournamentService {
     return this.http.get<Tournament[]>(this.tournamentDataUrl);
   }
 
+  createTournament(tournament: Tournament): Observable<Tournament> {
+    console.log(tournament);
+
+    return this.http.post<Tournament>(`${this.apiUrl}/create`, tournament);
+  }
+  
   getTournamentById(): Observable<TournamentDetails> {
     return this.http.get<TournamentDetails>(this.tournamentByIdDataUrl);
   }
