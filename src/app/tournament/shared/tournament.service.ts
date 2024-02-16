24df5dcdf1163906feaa430c178ef1fc5b8b19fc
Rémi Meslin <remi.meslin@guarani.fr>
2024-02-16 09:02:21 +0100

    [FEATURE] Ajout url API

--- src/app/tournament/shared/tournament.service.ts
+++ src/app/tournament/shared/tournament.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { Tournament } from '../models/tournament.model';
 import { Observable } from 'rxjs';
 import { TournamentDetails } from '../models/tournament-details.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TournamentService {
   private tournamentDataUrl = '../assets/list-tournament.model.json';
-  private apiUrl = 'http://localhost:8080/api/tournament/';
+  private apiUrl = `${environment.urlApi}` + "/tournaments";
   private tournamentByIdDataUrl = '../assets/tournament-details.model.json';
 
   constructor(private http: HttpClient) {}
@@ -21,7 +22,7 @@ export class TournamentService {
   createTournament(tournament: Tournament): Observable<Tournament> {
     console.log(tournament);
 
-    return this.http.post<Tournament>(`${this.apiUrl}/create`, tournament);
+    return this.http.post<Tournament>(`${this.apiUrl}`, tournament);
   }
   
   getTournamentById(): Observable<TournamentDetails> {
