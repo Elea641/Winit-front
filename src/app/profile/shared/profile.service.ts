8c54560cbfde818c5ea99ba47e66a871a68036d6
COLINE GUERIN <guerin.coline@gmail.com>
2024-02-16 09:25:48 +0100

    update profile put request

--- src/app/profile/shared/profile.service.ts
+++ src/app/profile/shared/profile.service.ts
@@ -1,8 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CurrentProfile } from '../models/current-profile.model';
 import { TeamMembers } from '../models/teamMembers.model';
+import {User} from "../../auth/models/user.model";
+import {environment} from "../../../environments/environment";
+import {LocalStorageService} from "../../auth/shared/local-storage.service";
 
 @Injectable({
   providedIn: 'root',
@@ -13,7 +16,10 @@ export class ProfileService {
 
   private teamMembersDataUrl = '../assets/list-team-members.model.json';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private lsService: LocalStorageService
+  ) {}
 
   getCurrentProfile(): Observable<CurrentProfile> {
     return this.http.get<CurrentProfile>(this.currentProfiletDataUrl);
@@ -21,5 +27,15 @@ export class ProfileService {
 
   getTeamMembers(): Observable<TeamMembers> {
     return this.http.get<TeamMembers>(this.teamMembersDataUrl);
+  }
+
+  updateProfile(userId: number, user: User): Observable<User> {
+    const token = this.lsService.getToken();
+    const headers = new HttpHeaders()
+      .set('Authorization', 'Bearer ' + token)
+      .set('Content-Type', 'application/json');
+    const options = { headers: headers };
+
+    return this.http.put<User>(`${environment.urlApi}/users/${userId}`, user, options);
   }
 }
