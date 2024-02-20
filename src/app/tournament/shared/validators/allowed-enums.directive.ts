227f16e97b1d4a5388dbd794f03e19e12c0f5b85
Rémi Meslin <remi.meslin@guarani.fr>
2024-02-20 16:07:21 +0100

    [FEATURE] Ajout des validateurs

---
+++ src/app/tournament/shared/validators/allowed-enums.directive.ts
@@ -0,0 +1,20 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+export function enumValidator(enumObject: any): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+        const value = control.value;
+        const enumValues = Object.values(enumObject);
+
+        if (value) {
+            for (let enumValue of enumValues) {
+
+                if (enumValue == value) {
+                    return null;
+                }
+            }
+            return { enum: true };
+        } else {
+            return { enum: true };
+        }
+    }
+}
