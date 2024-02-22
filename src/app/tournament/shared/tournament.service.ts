26f548f45adcbb7e486a44fc76e3bb163081e9a6
Rémi Meslin <remi.meslin@guarani.fr>
2024-02-22 16:43:29 +0100

    [FAETURE] Ajout méthode envoi tournament

--- src/app/tournament/shared/tournament.service.ts
+++ src/app/tournament/shared/tournament.service.ts
@@ -1,54 +1,67 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Tournament } from '../models/tournament.model';
-import { Observable, catchError, tap, throwError } from 'rxjs';
-import { TournamentDetails } from '../models/tournament-details.model';
-import { environment } from 'src/environments/environment';
-import { ToastService } from 'src/app/shared/toast.service';
 import { Router } from '@angular/router';
-import { AbstractControl } from '@angular/forms';
+
+import { Observable, throwError } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+import { Tournament } from '../models/tournament.model';
+import { TournamentDetails } from '../models/tournament-details.model';
+import { TournamentCreationDto } from '../models/tournament-creation-dto.model';
+
+import { ToastService } from 'src/app/shared/toast.service';
+import { TournamentMappers } from './mappers/TournamentMappers';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TournamentService {
   private tournamentDataUrl = '../assets/list-tournament.model.json';
-  private apiUrl = `${environment.urlApi}` + "/tournaments";
+  // private apiUrl = `${environment.urlApi}` + "/tournaments";
+  private apiUrl = `${environment.urlApi}` + "/tournaments/create";
   private tournamentByIdDataUrl = '../assets/tournament-details.model.json';
 
-  constructor(
-    private http: HttpClient,
-    private router: Router,
-    private toastService: ToastService) {}
+    constructor(
+      private http: HttpClient,
+      private router: Router,
+      private toastService: ToastService,
+      private tournamentMappers: TournamentMappers) {}
 
-  getAllTournaments(): Observable<Tournament[]> {
-    return this.http.get<Tournament[]>(this.tournamentDataUrl);
-  }
+    createTournament(newTournament: TournamentCreationDto): void {
+      const tournamentCreationDto = this.tournamentMappers.ToFormData(newTournament);
 
-  createTournament(newTournament: AbstractControl): Observable<Tournament> {
-    console.log(newTournament);
+      const headers = new HttpHeaders();
+      headers.append('Content-type', 'multipart/form-data');
 
-    return this.http.post<Tournament>(`${this.apiUrl}`, newTournament).pipe(
-      tap((response: Tournament) => {
-        console.log(response);
-        this.router.navigate(['/tournament/' + response.id]);
-        this.toastService.showSuccess(
-          "Votre tournoi est prêt !",
-          "Tournoi créé avec succès"
-        );
-      }),
-      catchError((error: any) => {
-        const errorMessage = error?.error?.error_message || 'Une erreur est survenue';
-        this.toastService.showError(
-          errorMessage,
-          "Erreur lors de la création du tournoi"
-        );
-        return throwError(() => new Error(error));
-      })
-    );
-  }
+      this.http.post<Tournament>(this.apiUrl, tournamentCreationDto, { headers }).subscribe(
+        (response) => {
+          console.log("Response: ", response);
+          if (response) {
+            console.log("success");
+            this.router.navigate(['/tournament/' + response.id]);
+            this.toastService.showSuccess(
+              "Votre tournoi est prêt !",
+              "Tournoi créé avec succès"
+            );
+          }
+        },
+        (error) => {
+          console.log("Error: ", error);
+          const errorMessage = error?.error?.error_message || 'Une erreur est survenue';
+          this.toastService.showError(
+            errorMessage,
+            "Erreur lors de la création du tournoi"
+          );
+          return throwError(() => new Error(error));
+        });
+    }
+
+    getAllTournaments(): Observable<Tournament[]> {
+      return this.http.get<Tournament[]>(this.tournamentDataUrl);
+    }
   
-  getTournamentById(): Observable<TournamentDetails> {
-    return this.http.get<TournamentDetails>(this.tournamentByIdDataUrl);
-  }
+    getTournamentById(): Observable<TournamentDetails> {
+      return this.http.get<TournamentDetails>(this.tournamentByIdDataUrl);
+    }
 }
