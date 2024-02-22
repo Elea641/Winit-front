4f9bd4ed313d56fa30f35e18c45c536e59657f2c
Rémi Meslin <remi.meslin@guarani.fr>
2024-02-22 16:41:26 +0100

    [FEATURE] Ajout méthode conversion

---
+++ src/app/tournament/shared/mappers/TournamentMappers.ts
@@ -0,0 +1,63 @@
+import { Injectable } from "@angular/core";
+import { TournamentForm } from "../../models/tournament-form.model";
+import { TournamentCreationDto } from "../../models/tournament-creation-dto.model";
+
+@Injectable({
+    providedIn: 'root',
+})
+export class TournamentMappers {
+    ToCreationDto(model: TournamentForm["form"]): TournamentCreationDto {
+
+        console.log(new Date(model.controls["date"].value));
+
+        const tournament: TournamentCreationDto = {
+            name: model.controls["name"].value,
+            date: model.controls["date"].value,
+            place: model.controls["place"].value,
+            sport: model.controls["sport"].value,
+            playersPerTeam: model.controls["playersPerTeam"].value,
+            minTeams: model.controls["minTeams"]?.value,
+            maxTeams: model.controls["maxTeams"].value,
+            gameLength: model.controls["gameLength"].value,
+            privacy: model.controls["privacy"].value,
+            playerCategory: model.controls["playerCategory"].value,
+            tournamentFormat: model.controls["tournamentFormat"].value,
+            tournamentBanner: model.controls["tournamentBanner"]?.value,
+            inscriptionLimitDate: model.controls["inscriptionLimitDate"]?.value,
+        };
+
+        return tournament;
+    }
+
+    ToFormData(model: TournamentCreationDto): FormData {
+        const formData = new FormData();
+
+        console.log("Value : ", model.sport);
+
+        formData.append('name', model.name);
+        formData.append('date', model.date.toString());
+        formData.append('place', model.place);
+        formData.append('sportName', model.sport);
+        console.log("FormData Sport : ", formData.get('sport'));
+        formData.append('playersPerTeam', String(model.playersPerTeam));
+        formData.append('maxTeams', String(model.maxTeams));
+        formData.append('gameLength', String(model.gameLength));
+        formData.append('privacy', model.privacy);
+        formData.append('playerCategory', model.playerCategory);
+        formData.append('tournamentFormat', model.tournamentFormat);
+
+        if (model.inscriptionLimitDate) {
+            formData.append('inscriptionLimitDate', model.inscriptionLimitDate.toString());
+        }
+
+        if (model.minTeams) {
+            formData.append('minTeams', String(model.minTeams));
+        }
+        
+        if (model.tournamentBanner) {
+            formData.append('tournamentBanner', model.tournamentBanner);
+        }
+
+        return formData; 
+    }
+}
