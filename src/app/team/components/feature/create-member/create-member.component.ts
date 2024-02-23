3f445b4faa9f72ec9bfff1f2373d78c8d8e6982c
Elea641 <eleavoliotis64@gmail.com>
2024-02-23 09:08:00 +0100

    wip add team in profile

--- src/app/team/components/feature/create-member/create-member.component.ts
+++ src/app/team/components/feature/create-member/create-member.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import {
   FormControl,
   FormGroup,
@@ -15,6 +15,9 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { CurrentUser } from 'src/app/auth/models/current-user.model';
+import { Observable, map, startWith } from 'rxjs';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-create-member',
@@ -28,6 +31,8 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router';
     FormsModule,
     MatDividerModule,
     RouterModule,
+    MatAutocompleteModule,
+    AsyncPipe,
   ],
   templateUrl: './create-member.component.html',
   styleUrls: ['./create-member.component.scss'],
@@ -36,6 +41,8 @@ export class CreateMemberComponent {
   memberForm!: FormGroup;
   member: Member = new Member('');
   teamName: string = '';
+  users: CurrentUser[] = [];
+  filteredUsers!: Observable<CurrentUser[]>;
 
   constructor(
     public teamService: TeamService,
@@ -56,6 +63,23 @@ export class CreateMemberComponent {
 
     this.route.parent?.params.subscribe((params) => {
       this.teamName = params['teamName'];
+    });
+
+    this.route.data.subscribe(({ user }) => {
+      this.users = user;
+    });
+
+    this.filteredUsers = this.memberForm.controls['name'].valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): CurrentUser[] {
+    const filterValue = value.toLowerCase();
+    return this.users.filter((user: CurrentUser) => {
+      const fullName = `${user.firstName} ${user.lastName}`.toLowerCase();
+      return fullName.includes(filterValue);
     });
   }
 
--- src/app/team/components/feature/member-detail/member-detail.component.ts
+++ src/app/team/components/feature/member-detail/member-detail.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListMemberComponent } from '../list-member/list-member.component';
 import { CreateMemberComponent } from '../create-member/create-member.component';
 import { MenuButtonsComponent } from 'src/app/components/ui/menu-buttons/menu-buttons.component';
+import { Team } from 'src/app/team/models/team.model';
+import { MatTabsModule } from '@angular/material/tabs';
 @Component({
   selector: 'app-member-detail',
   standalone: true,
@@ -11,12 +13,18 @@ import { MenuButtonsComponent } from 'src/app/components/ui/menu-buttons/menu-bu
     ListMemberComponent,
     CreateMemberComponent,
     MenuButtonsComponent,
+    MatTabsModule,
   ],
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.scss'],
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit {
+  @Input() selectedTeam: Team | null = null;
   buttonValueClicked: string = 'list-member';
+
+  ngOnInit(): void {
+    console.log(this.selectedTeam);
+  }
 
   onButtonClicked(label: string) {
     this.buttonValueClicked = label;
