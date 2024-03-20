577dcd9b929a64e526829424b0246eca6a50efab
COLINE GUERIN <guerin.coline@gmail.com>
2024-03-20 11:19:40 +0100

    back-office page

--- src/app/back-office/pages/back-office-page/back-office-page.component.ts
+++ src/app/back-office/pages/back-office-page/back-office-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {BackOfficeLayoutComponent} from "../../components/ui/back-office-layout/back-office-layout.component";
 
 @Component({
   selector: 'app-back-office-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BackOfficeLayoutComponent],
   templateUrl: './back-office-page.component.html',
   styleUrls: ['./back-office-page.component.scss']
 })
