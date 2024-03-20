9bfe8e3be3e80d4e756f995ab9a3e8ba1623d300
Elea641 <eleavoliotis64@gmail.com>
2024-03-20 14:57:06 +0100

    add menu created team

---
+++ src/app/team/pages/create-team-page/create-team-page.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateTeamPageComponent } from './create-team-page.component';
+
+describe('CreateTeamPageComponent', () => {
+  let component: CreateTeamPageComponent;
+  let fixture: ComponentFixture<CreateTeamPageComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CreateTeamPageComponent]
+    });
+    fixture = TestBed.createComponent(CreateTeamPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
