9d7178456dfc0db0829fe6f25b888714b4418e13
COLINE GUERIN <guerin.coline@gmail.com>
2024-04-19 11:12:23 +0200

    back-office sport model

---
+++ src/app/back-office/models/admin-sport.model.ts
@@ -0,0 +1,9 @@
+export class AdminSport {
+  id?: number;
+  position?: number;
+  name?: string;
+  numberOfPlayers?: number;
+  imageUrl?: string;
+  numberOfTeams?: number;
+  numberOfTournaments?: number;
+}
