bb96c0b47ffe8823a9594eeed6a2a460fdf42b18
COLINE GUERIN <guerin.coline@gmail.com>
2024-04-19 11:12:57 +0200

    back-office user model

---
+++ src/app/back-office/models/admin-user.model.ts
@@ -0,0 +1,13 @@
+export class AdminUser {
+  position?: number;
+  firstName?: string;
+  lastName?: string;
+  dateOfBirth?: Date;
+  email?: string;
+  city?: string;
+  isEnabled?: boolean;
+  role?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  isActive?: string;
+}
