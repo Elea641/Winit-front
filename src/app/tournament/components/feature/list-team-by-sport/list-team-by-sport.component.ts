21c8a0fdcc1a2cabcbc234b7a682bbc82eef1b41
KevinDavoust <kevin.davoust@orange.fr>
2024-05-15 12:22:01 +0200

    prettier

--- src/app/tournament/components/feature/list-team-by-sport/list-team-by-sport.component.ts
+++ src/app/tournament/components/feature/list-team-by-sport/list-team-by-sport.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeamCardComponent } from 'src/app/profile/components/ui/team-card/team-card.component';
 import { Team } from 'src/app/team/models/team.model';
@@ -9,7 +9,7 @@ import { Team } from 'src/app/team/models/team.model';
   templateUrl: './list-team-by-sport.component.html',
   styleUrls: ['./list-team-by-sport.component.scss'],
 })
-export class ListTeamBySportComponent {
+export class ListTeamBySportComponent implements OnInit {
   @Input() teams!: Team[] | null;
 
   validTeams!: Team[] | undefined;
