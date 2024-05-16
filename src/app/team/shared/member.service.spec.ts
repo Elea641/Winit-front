d88b60c079ad7b1e209362d12572a239a6a8b286
Elea641 <eleavoliotis64@gmail.com>
2024-05-16 14:58:40 +0200

    add test member service

--- src/app/team/shared/member.service.spec.ts
+++ src/app/team/shared/member.service.spec.ts
@@ -2,21 +2,77 @@ import { TestBed } from '@angular/core/testing';
 
 import { MemberService } from './member.service';
 import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { ToastService } from 'src/app/shared/toast.service';
+import { Member } from '../models/member.model';
 
 describe('MemberService', () => {
   let service: MemberService;
+  let memberServiceSpy: jasmine.SpyObj<MemberService>;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
+    const spy = jasmine.createSpyObj('TeamService', [
+      'addMember',
+      'deleteMemberByTeamName',
+    ]);
+
     TestBed.configureTestingModule({
-      imports: [HttpClientModule, ToastrModule.forRoot()],
-      providers: [ToastService, ToastrService],
+      imports: [
+        HttpClientModule,
+        HttpClientTestingModule,
+        ToastrModule.forRoot(),
+      ],
+      providers: [
+        ToastService,
+        ToastrService,
+        { provide: MemberService, useValue: spy },
+      ],
     });
     service = TestBed.inject(MemberService);
+    memberServiceSpy = TestBed.inject(
+      MemberService
+    ) as jasmine.SpyObj<MemberService>;
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add member', () => {
+    const mockMember: Member = {
+      id: 1,
+      firstName: 'User1',
+      lastName: 'User',
+    };
+
+    memberServiceSpy.addMember.and.returnValue();
+
+    memberServiceSpy.addMember('Team A', mockMember);
+
+    expect(memberServiceSpy.addMember.calls.count())
+      .withContext('spy method was called once')
+      .toBe(1);
+  });
+
+  it('should delete member', () => {
+    const mockMember: Member = {
+      id: 1,
+      firstName: 'User1',
+      lastName: 'User',
+    };
+
+    memberServiceSpy.deleteMemberByTeamName.and.returnValue();
+
+    memberServiceSpy.deleteMemberByTeamName('Team A', mockMember);
+
+    expect(memberServiceSpy.deleteMemberByTeamName.calls.count())
+      .withContext('spy method was called once')
+      .toBe(1);
   });
 });
