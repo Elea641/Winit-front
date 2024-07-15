a6de2785a47527e55c8b274f592d92fde24d168b
KevinDavoust <kevin.davoust@orange.fr>
2024-07-15 10:07:13 +0200

    created sidebar components

---
+++ src/app/components/ui/sidebar/sidebar-mobile/sidebar-mobile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sidebar-mobile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sidebar-mobile.component.html',
+  styleUrls: ['./sidebar-mobile.component.scss'],
+})
+export class SidebarMobileComponent {}
